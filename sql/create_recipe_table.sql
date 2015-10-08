04175cc0dd7b339741d721ade4ce4a59bbeca581
Evgeni Vasilev <evgeni.vasilev0912@gmail.com>
2015-10-08 21:48:11 +0300

    Initial commit

--- sql/create_recipe_table.sql
+++ sql/create_recipe_table.sql
@@ -1,7 +1,7 @@
 create table recipes (
   recipe_id int (11) AUTO_INCREMENT PRIMARY KEY,
   recipe_name varchar (255) NOT NULL DEFAULT  "Recipe Name",
-  recipe_description varchar (255) NOT NULL Default "Recipe Description",
+  recipe_description varchar (255) NOT NULL DEFAULT "Recipe Description",
   recipe_body  text,
   recipe_image varchar (50) NOT NULL DEFAULT  "food.jpg"
 );
